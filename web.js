e674fc1f843763e9b24737a142f0fa45047bdb6d
thinh-huynh2004 <thinh.huynhhctbk22@hcmut.edu.vn>
2025-08-01 17:17:59 +0700

    1/8/2025

--- web.js
+++ web.js
@@ -25,9 +25,24 @@ const myModule = (function() {
         themeBtn.addEventListener("click", () => changeTheme());
     }
 
-    function fetchData() {
+    // function fetchData() {
+    //     const url = "https://jsonplaceholder.typicode.com/posts";
+    //     fetch(url).then(response => response.json())
+    //     .then(data => {
+    //         const ul = document.getElementById("main__ul");
+    //         for (let i = 0; i < 3; i++) {
+    //             const li = document.createElement("li");
+    //             li.classList.add("text-16", "py-10");
+    //             li.innerHTML = `${data[i].title} <br> ${data[i].body}`;
+    //             ul.appendChild(li);
+    //         }
+    //     })
+    //     .catch(error => console.log(error));
+    // }
+
+    async function fetchData() {
         const url = "https://jsonplaceholder.typicode.com/posts";
-        fetch(url).then(response => response.json())
+        await fetch(url).then(response => { return response.json()})
         .then(data => {
             const ul = document.getElementById("main__ul");
             for (let i = 0; i < 3; i++) {
@@ -37,6 +52,7 @@ const myModule = (function() {
                 ul.appendChild(li);
             }
         })
+        .catch(error => {return console.log(error)});
     }
 
     return {
